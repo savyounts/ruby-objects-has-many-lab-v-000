032e87662a72c4cc389814e1fcda7fa2d35d44e5
Sav Younts <savyounts@gmail.com>
2018-08-09 17:38:56 +0000

    automatically backed up by learn

--- lib/author.rb
+++ lib/author.rb
@@ -1,19 +1,27 @@
 class Author 
   attr_accessor :name 
-  
+  @@all_posts
   def initialized(name)
     @name = name
     @posts = []
   end 
   
   def add_post(post)
-    post = Post.new(title)
-    @posts << post 
+    @posts << post
+    @@all_posts << post
     post.author = self 
     post
   end 
   
   def add_post_by_title(title)
-    
+    post = Post.new(title)
+    @posts << post 
+    @@all_posts << post
+    post.author = self 
+    post 
+  end
   
+  def post_count
+    @@all_posts
+  end
 end
