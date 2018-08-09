abff6adbacacae77eb69cbfbc3ff171c9e866a14
Sav Younts <savyounts@gmail.com>
2018-08-09 17:43:59 +0000

    automatically backed up by learn

--- lib/post.rb
+++ lib/post.rb
@@ -1,8 +1,26 @@
 class Post 
-  attr_accessor :title 
+  attr_accessor :title, :author
   @@all = []
   def initialize(title)
     @title = title
-    
+    @@all << title 
   end
+  
+  def all 
+    @@all
+  end 
+  
+  def author 
+    self.author = Author.new(name)
+  end 
+  
+  def author_name
+    if self.author == nil 
+      nil
+    else 
+      self.author.name
+    end
+  end
+ 
+  
 end
