96708c92834553e92d5817a321d55c6012cf4586
Sav Younts <savyounts@gmail.com>
2018-08-09 18:00:20 +0000

    Done.

---
+++ lib/artist.rb
@@ -0,0 +1,34 @@
+class Artist 
+  attr_accessor :name 
+  
+  @@all = []
+  
+  def initialize(name)
+    @name = name
+    @songs = []
+  end 
+    
+  def add_song(song)
+    @songs << song
+    @@all << song
+    song.artist = self 
+    song 
+  end 
+  
+  def add_song_by_name(name)
+    song = Song.new(name)
+    @songs << song 
+    @@all << song 
+    song.artist = self 
+    song
+  end 
+  
+  def songs
+    @songs
+  end 
+  
+  def self.song_count
+    @@all.size
+  end
+
+end 
---
+++ lib/author.rb
@@ -0,0 +1,32 @@
+class Author 
+  attr_accessor :name, :author
+  @@all = []
+  
+  def initialize(name)
+    @name = name
+    @posts = []
+  end 
+  
+  def add_post(article)
+    @posts << article
+    @@all << article
+    article.author = self 
+    article
+  end 
+  
+  def posts 
+    @posts 
+  end 
+  
+  def add_post_by_title(title)
+    article = Post.new(title)
+    @posts << article 
+    @@all << article
+    article.author = self
+    article 
+  end
+  
+  def self.post_count
+    @@all.size
+  end
+end
