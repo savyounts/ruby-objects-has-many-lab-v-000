59a70d09b5693dc4dc0988784a79f2d792e8f8fd
Sav Younts <savyounts@gmail.com>
2018-08-09 17:25:51 +0000

    automatically backed up by learn

---
+++ lib/artist.rb
@@ -0,0 +1,34 @@
+class Artist 
+  attr_accessor :name 
+  
+  @@all = []
+  
+  def initialize(name)
+    @name = name
+    @songs = []
+  end 
+  
+  def add_song_by_name(name)
+    song = Song.new(name)
+    @songs << song 
+    @@all << song 
+    song.artist = self 
+    song
+  end 
+  
+  def add_song(song)
+    song = Song.new(song)
+    @songs << song
+    song.artist = self 
+    song 
+  end 
+  
+  def songs
+    @songs
+  end 
+  
+  def self.song_count
+    @@all.size
+  end
+
+end 
